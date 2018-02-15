9296b18d25058fe8bf6539c04b78ab17edffd849
Lamia Mamlouk <lamia.mamlouk@gmail.com>
2018-02-15 16:38:18 +0100

    exerice 1 sans css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+Ingredient.destroy_all
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+list_serialized = open(url).read
+list = JSON.parse(list_serialized)
+
+list['drinks'].each do |drink|
+  Ingredient.create!(name: drink['strIngredient1'].capitalize)
+end
