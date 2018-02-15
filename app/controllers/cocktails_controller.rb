63a5537e7133e3e08eb976a0a331f5d05622a3df
Lamia Mamlouk <lamia.mamlouk@gmail.com>
2018-02-15 17:05:29 +0100

    exercicie fini ok

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -16,8 +16,11 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
+    if @cocktail.save
     redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
 
   end
 
